43fb3050f41bb5287ffead7ac5f7b50a8922f4f8
Srinivasulu Dammu <Dammu.10741834@LTIMindtree.com>
2024-06-13 16:51:43 +0530

    New edit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node 8!\n'
+  const msg = 'Hello Node 9!\n'
   res.end(msg);
 });
 
