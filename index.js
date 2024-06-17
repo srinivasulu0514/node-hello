9eb43afd6568fa602e24e1772d0c9f0199a7d276
Srinivasulu Dammu <Dammu.10741834@LTIMindtree.com>
2024-06-17 12:34:29 +0530

    Changes form Vasu to Srinivasulu

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = '<h1>Hello Srinivas!<h1>\n'
+  const msg = '<h1>Hello Srinivasulu!<h1>\n'
   res.end(msg);
 });
 
